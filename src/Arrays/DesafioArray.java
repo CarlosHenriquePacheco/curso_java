db528568c49af36790eeb3606d830b74c18027bd
Henrique Pacheco <carlos-henrique.pacheco@serpro.gov.br>
2023-10-02 19:26:11 -0300

    Inserindo Classe Desafio

---
+++ src/Arrays/DesafioArray.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DesafioArray {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a quantidade de alunos:");
+        int vQtdAlunos = Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite a quantidade de notas");
+        int vQtdNotas = Integer.parseInt(entrada.nextLine());
+
+        double[][] vNotas = new double[vQtdAlunos][vQtdNotas];
+
+        for (int i = 0; i < vQtdAlunos; i++) {
+            for (int j = 0; j < vQtdNotas; j++) {
+                System.out.println("Digite a nota "+ (j+1) +" do aluno "+ (i+1)+":");
+                vNotas[i][j] = Double.parseDouble(entrada.nextLine());    
+            }
+        }
+        double total = 0.0;
+        double media = 0.0;
+        for (int i = 0; i < vQtdAlunos; i++) {
+            for (double nota : vNotas[i]) {
+            total = total + nota;
+            }    
+            media = total/vQtdNotas;    
+            System.out.println("Array:"+Arrays.toString(vNotas[i]));
+            System.out.printf("Media : %.2f",media);
+            System.out.println();
+            total = 0.0;
+            media = 0.0;
+        }
+        entrada.close();
+        
+    }
+}
