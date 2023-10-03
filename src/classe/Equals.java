4b3b9ded4e254d17105829e132a941bf30ba23de
Henrique Pacheco <carlos-henrique.pacheco@serpro.gov.br>
2023-10-03 11:44:09 -0300

    trocando nome classe

--- src/classe/Equals.java
+++ src/classe/Equals.java
@@ -1,6 +1,8 @@
 package classe;
 
-public class Equals_Hashcode {
+import java.util.Date;
+
+public class Equals {
     public static void main(String[] args) {
         Usuario u1 = new Usuario();
         u1.setNome("Carlos Henrique");
@@ -13,6 +15,6 @@ public class Equals_Hashcode {
         System.out.println(u1==u2);
         System.out.println(u1.equals(u2));
         System.out.println(u2.equals(u1));
+        System.out.println(u2.equals(new Date()));
     }
-    
 }
