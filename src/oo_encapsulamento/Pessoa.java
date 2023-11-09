f84cf2525b2989efe2c1430a7836a034cc52d5f0
Henrique Pacheco <carlos-henrique.pacheco@serpro.gov.br>
2023-11-08 23:12:33 -0300

    Correcao Getters and Setters

--- src/oo_encapsulamento/Pessoa.java
+++ src/oo_encapsulamento/Pessoa.java
@@ -1,19 +1,45 @@
 package oo_encapsulamento;
 
 public class Pessoa {
+    private String nome;
+    private String sobrenome;
     private int idade;
     //Construtor
-    public Pessoa(int idade) {
-        this.idade = idade;
+    public Pessoa(String nome,String sobrenome,int idade) {
+        setNome(nome);
+        setSobrenome(sobrenome);
+        setIdade(idade);
     }
     //Getters and Setters
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public int getIdade() {
         return idade;
     }
 
     public void setIdade(int pIdade) {
-        if (pIdade>=0) {
+        pIdade = Math.abs(pIdade);
+        if (pIdade>=0 && pIdade <=120) {
             this.idade = pIdade;    
         }
+    }
+    public String getNomeCompleto(){
+        return getNome() + " "+ getSobrenome();
+    }
+
+    @Override
+    public String toString(){
+        return "Ola, eu sou o "+getNome()+ " "+getSobrenome()+ " e tenho "+getIdade()+" anos!";
     }
 }
--- src/oo_encapsulamento/PessoaTeste.java
+++ src/oo_encapsulamento/PessoaTeste.java
@@ -2,7 +2,10 @@ package oo_encapsulamento;
 
 public class PessoaTeste {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(48);
+        Pessoa p1 = new Pessoa("Henrique","Pacheco",48);
         //p1.idade = -30; não pode acessar atributo private
+        System.out.println(p1.getNomeCompleto());
+        System.out.println(p1.getIdade());
+        System.out.println(p1); //Chama o overhide toString 
     }
 }
