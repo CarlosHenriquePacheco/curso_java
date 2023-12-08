8447a4fc27b0a571e6d81c397efb454729cbc515
Henrique Pacheco <carlos-henrique.pacheco@serpro.gov.br>
2023-12-08 15:43:52 -0300

    predicate e Consumer Java

--- src/lambdas/ForEachLambda.java
+++ src/lambdas/ForEachLambda.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class ForEachLambda {
     public static void main(String[] args) {
         List<String> aprovados = Arrays.asList("Ana","Bia","Lia","Gui");
-        System.out.prgit add .addintln("Aprovados Forma Tradicional:");
+        System.out.println("Aprovados Forma Tradicional:");
         for (String nome : aprovados) {
             System.out.println(nome);
         }
@@ -14,5 +14,14 @@ public class ForEachLambda {
 
         System.out.println("Method Reference...");
         aprovados.forEach(System.out::println);
+
+        System.out.println("Lambda #02...");
+        aprovados.forEach((nome) -> {meuimprimir(nome);});
+
+        System.out.println("Method Reference2...");
+        aprovados.forEach(ForEachLambda::meuimprimir);
+    }
+    static void meuimprimir(String nome){
+        System.out.println("Oi! Meu nome é "+nome);
     }
 }
---
+++ src/lambdas/Predicado.java
@@ -0,0 +1,11 @@
+package lambdas;
+import java.util.function.Predicate;
+
+public class Predicado {
+    public static void main(String[] args) {
+        Predicate<Produto> isCaro = prod -> ((prod.preco)* (1-prod.desconto)) >=1000;
+
+        Produto produto = new Produto("Notebook", 1200.00, 0.15);
+        System.out.println(isCaro.test(produto));
+    }
+}
