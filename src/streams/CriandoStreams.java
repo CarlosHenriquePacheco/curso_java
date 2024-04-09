929245c167495b395074a831031a2042005014d4
Henrique Pacheco <carlos-henrique.pacheco@serpro.gov.br>
2024-04-09 14:32:40 -0300

    streams inicial

---
+++ src/streams/CriandoStreams.java
@@ -0,0 +1,24 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class CriandoStreams {
+    public static void main(String[] args) {
+        Consumer<String> print = System.out::print;
+
+        Stream<String> langs = Stream.of("Java ","Delphi ","JS\n");
+        langs.forEach(print);
+
+        String[] maislangs = {"Pyton ","Lisp ","Perl ","Go\n"};
+        Stream.of(maislangs).forEach(print);
+        Arrays.stream(maislangs).forEach(print);
+        Arrays.stream(maislangs,1,3).forEach(print);
+        System.out.println(" ");
+        List<String> outraslangs = Arrays.asList("C ","PHP ","Kotlin");
+        outraslangs.stream().forEach(print);
+        outraslangs.parallelStream().forEach(print);
+    }
+}
--- src/streams/imprimindoObjetos.java
+++ src/streams/imprimindoObjetos.java
@@ -9,19 +9,21 @@ public class imprimindoObjetos {
     public static void main(String[] args) {
         List<String> aprovados = Arrays.asList("Lu","Gui","Ana");
         //Usando Foreach
-        System.out.println("\nUsando Foreach");
+        System.out.println("\nUsando Foreach...");
         for (String nome : aprovados) {
             System.out.println(nome);            
         }
         //Usando Iterator
-        System.out.println("\nUsando Iterator");
+        System.out.println("\nUsando Iterator...");
         Iterator<String> iterator = aprovados.iterator();
         while(iterator.hasNext()){
             System.out.println(iterator.next());
         }
 
         //Usando Stream
+         //Usando Iterator
+         System.out.println("\nUsando Stream...");
         Stream<String> stream = aprovados.stream();
-        stream.forEach(System.out::println);   //Laço interno
+        stream.forEach(System.out::println);   //Laço interno!!!
     }
 }
